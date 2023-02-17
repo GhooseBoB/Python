058504a6380853ac285561987de3cfbc992d75fa
GhooseBoB <125651383+GhooseBoB@users.noreply.github.com>
2023-02-17 17:17:12 +0700

    งานEp2

---
+++ Homework2.py
@@ -0,0 +1,15 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter import messagebox
+GUI = Tk()
+GUI.title('game')
+GUI.geometry('500x400')
+
+B1 = ttk.Button(GUI,text='rov')
+B1.pack(ipadx=20,ipady=20)
+B2 = ttk.Button(GUI,text='roblox')
+B2.pack(ipadx=20,ipady=20)
+B3 = ttk.Button(GUI,text='Among us')
+B3.pack(ipadx=20,ipady=20)
+B4 = ttk.Button(GUI,text='freefire')
+B4.pack(ipadx=20,ipady=20)
